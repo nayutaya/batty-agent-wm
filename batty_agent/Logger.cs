0e7b2fc2b1be1963ed00ec694fa697b85a0403d6
Yuya Kato <yuyakato@gmail.com>
2009-07-08 11:37:43 +0900

    Loggerクラスを追加
    * logディレクトリの初期化処理を追加
    * logファイルの試験的な書き込み処理を追加

---
+++ batty_agent/Logger.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace nayutaya.batty.agent
+{
+    class Logger
+    {
+        private DirectoryInfo logdir = null;
+
+        public Logger()
+        {
+            DirectoryInfo rootdir = new DirectoryInfo(this.GetExecutingAssemblyDirectoryPath());
+            this.logdir = rootdir.CreateSubdirectory("log");
+
+            this.Write(DateTime.Now, 0);
+        }
+
+        public void Write(DateTime time, byte level)
+        {
+            string timestr = time.ToString("yyyyMMdd");
+            string filepath = this.logdir.FullName + "\\" + timestr + ".txt";
+
+            using ( StreamWriter writer = File.AppendText(filepath) )
+            {
+                writer.WriteLine("hoge");
+            }
+        }
+
+
+        // TODO: ユーティリティクラスに移動する
+        private string GetExecutingAssemblyFilePath()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly().ManifestModule.FullyQualifiedName;
+        }
+
+        // TODO: ユーティリティクラスに移動する
+        private string GetExecutingAssemblyDirectoryPath()
+        {
+            return System.IO.Path.GetDirectoryName(this.GetExecutingAssemblyFilePath());
+        }
+    }
+}
