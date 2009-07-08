8f7da0bfc700abd20f0950cc4a111d7ea504cc01
Yuya Kato <yuyakato@gmail.com>
2009-07-08 14:55:24 +0900

    レコードの初期化処理と保存処理を追加

--- batty_agent/Record.cs
+++ batty_agent/Record.cs
@@ -7,5 +7,39 @@ namespace nayutaya.batty.agent
 {
     class Record
     {
+        /// <summary>記録日時</summary>
+        private DateTime time;
+        /// <summary>バッテリレベル</summary>
+        private byte level;
+        /// <summary>送信済み</summary>
+        private bool sent;
+
+        public Record(DateTime time, byte level, bool sent)
+        {
+            this.time = time;
+            this.level = level;
+            this.sent = sent;
+        }
+
+        public Record(DateTime time, byte level)
+            : this(time, level, false)
+        {
+            // nop
+        }
+
+        public DateTime Time
+        {
+            get { return time; }
+        }
+
+        public byte Level
+        {
+            get { return level; }
+        }
+
+        public bool Sent
+        {
+            get { return sent; }
+        }
     }
 }
--- batty_agent/RecordStorage.cs
+++ batty_agent/RecordStorage.cs
@@ -2,10 +2,21 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace nayutaya.batty.agent
 {
     class RecordStorage
     {
+        public static void Save(StreamWriter writer, List<Record> records)
+        {
+            foreach ( Record record in records )
+            {
+                string time = record.Time.ToString("yyyyMMddHHmmss");
+                string level = record.Level.ToString("D3");
+                string sent = (record.Sent ? "1" : "0");
+                writer.WriteLine("{0}{1}{2}", time, level, sent);
+            }
+        }
     }
 }
