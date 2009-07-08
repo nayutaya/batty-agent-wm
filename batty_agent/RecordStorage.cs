191c659ed55e497900075ba477522b1f0505b889
Yuya Kato <yuyakato@gmail.com>
2009-07-08 15:17:32 +0900

    リファクタリング

--- batty_agent/RecordStorage.cs
+++ batty_agent/RecordStorage.cs
@@ -9,11 +9,14 @@ namespace nayutaya.batty.agent
 {
     class RecordStorage
     {
+        private static readonly string DateFormat = "yyyyMMddHHmmss";
+        private static readonly Regex LinePattern = new Regex(@"^(?<time>\d{14})(?<level>\d{3})(?<sent>\d)$");
+
         public static void Save(StreamWriter writer, List<Record> records)
         {
             foreach ( Record record in records )
             {
-                string time = record.Time.ToString("yyyyMMddHHmmss");
+                string time = record.Time.ToString(DateFormat);
                 string level = record.Level.ToString("D3");
                 string sent = (record.Sent ? "1" : "0");
                 writer.WriteLine("{0}{1}{2}", time, level, sent);
@@ -27,12 +30,10 @@ namespace nayutaya.batty.agent
             while ( !reader.EndOfStream )
             {
                 string line = reader.ReadLine();
-
-                Regex pattern = new Regex(@"^(?<time>\d{14})(?<level>\d{3})(?<sent>\d)$");
-                Match match = pattern.Match(line);
+                Match match = LinePattern.Match(line);
                 if ( match.Success )
                 {
-                    DateTime time = DateTime.ParseExact(match.Groups["time"].Value, "yyyyMMddHHmmss", null);
+                    DateTime time = DateTime.ParseExact(match.Groups["time"].Value, DateFormat, null);
                     byte level = Byte.Parse(match.Groups["level"].Value);
                     bool sent = (match.Groups["sent"].Value == "0" ? false : true);
 
