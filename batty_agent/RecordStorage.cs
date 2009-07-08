08130acbd7966dee29ad79dc9821c5db341033cb
Yuya Kato <yuyakato@gmail.com>
2009-07-08 15:12:41 +0900

    レコードの読み込み処理を追加

--- batty_agent/RecordStorage.cs
+++ batty_agent/RecordStorage.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.IO;
 
 namespace nayutaya.batty.agent
@@ -17,6 +18,29 @@ namespace nayutaya.batty.agent
                 string sent = (record.Sent ? "1" : "0");
                 writer.WriteLine("{0}{1}{2}", time, level, sent);
             }
+        }
+
+        public static List<Record> Load(StreamReader reader)
+        {
+            List<Record> records = new List<Record>();
+
+            while ( !reader.EndOfStream )
+            {
+                string line = reader.ReadLine();
+
+                Regex pattern = new Regex(@"^(?<time>\d{14})(?<level>\d{3})(?<sent>\d)$");
+                Match match = pattern.Match(line);
+                if ( match.Success )
+                {
+                    DateTime time = DateTime.ParseExact(match.Groups["time"].Value, "yyyyMMddHHmmss", null);
+                    byte level = Byte.Parse(match.Groups["level"].Value);
+                    bool sent = (match.Groups["sent"].Value == "0" ? false : true);
+
+                    records.Add(new Record(time, level, sent));
+                }
+            }
+
+            return records;
         }
     }
 }
