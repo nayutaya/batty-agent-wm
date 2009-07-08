1eccc4f4ca2df4e18ad3aa6e3ef9f015abe6addc
Yuya Kato <yuyakato@gmail.com>
2009-07-08 15:23:50 +0900

    レコードマネージャに件数上限付きの保存処理を追加

--- batty_agent/RecordManager.cs
+++ batty_agent/RecordManager.cs
@@ -7,5 +7,35 @@ namespace nayutaya.batty.agent
 {
     class RecordManager
     {
+        private const int MaxOfRecords = 100;
+        private List<Record> records = null;
+
+        public RecordManager()
+        {
+            this.records = new List<Record>();
+        }
+
+        public void Add(Record record)
+        {
+            this.records.Add(record);
+
+            while ( this.records.Count > MaxOfRecords )
+            {
+                this.records.RemoveAt(0);
+            }
+        }
+
+        /*
+        using ( System.IO.StreamWriter writer = System.IO.File.CreateText(@"\Program Files\batty_agent\test.txt") )
+        {
+            RecordStorage.Save(writer, records);
+        }
+         */
+        /*
+        using ( System.IO.StreamReader reader = System.IO.File.OpenText(@"\Program Files\batty_agent\test.txt") )
+        {
+            List<Record> records2 = RecordStorage.Load(reader);
+        }
+         */
     }
 }
